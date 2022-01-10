5e169a4c6067a51d3fd7c728f077c56f8d21909b
kazuho cryershinozuka <cryershinozuka.kb@om.asahi-kasei.co.jp>
2022-01-10 11:12:01 +0900

    [setup] aws

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,46 +1,47 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-import Amplify from "aws-amplify";
-import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import Amplify from 'aws-amplify'
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
 import {
   AmplifyAuthContainer,
   AmplifyAuthenticator,
   AmplifySignIn,
-} from "@aws-amplify/ui-react";
-import { getTime } from "./api";
+} from '@aws-amplify/ui-react'
+import { getTime } from './api'
 
 Amplify.configure({
   Auth: {
-    region: "ap-northeast-1",
-    userPoolId: "ap-northeast-1_xxx", // Please change this value.
-    userPoolWebClientId: "xxx", // Please change this value.
+    region: 'ap-northeast-1',
+    userPoolId: 'ap-northeast-1_izW3RmRoB', // Please change this value.
+    userPoolWebClientId: '52gf758did0kunvlauh8norvfq', // Please change this value.
   },
-});
+})
 
-export const apiEndpoint = "https://xxx.ap-northeast-1.amazonaws.com"; // Please change this value. (Don't include '/api')
+export const apiEndpoint =
+  'https://r4rj7apo23.execute-api.ap-northeast-1.amazonaws.com' // Please change this value. (Don't include '/api')
 
 const App: React.FC = () => {
-  const [authState, setAuthState] = useState<AuthState>();
-  const [user, setUser] = useState<object | undefined>();
-  const [time, setTime] = useState<string>();
+  const [authState, setAuthState] = useState<AuthState>()
+  const [user, setUser] = useState<object | undefined>()
+  const [time, setTime] = useState<string>()
 
   useEffect(() => {
     return onAuthUIStateChange((nextAuthState, authData) => {
-      setAuthState(nextAuthState);
-      setUser(authData);
-    });
-  }, []);
+      setAuthState(nextAuthState)
+      setUser(authData)
+    })
+  }, [])
 
   useEffect(() => {
     const _getTime = async () => {
-      const res = await getTime();
-      setTime(res.cur_date);
-    };
+      const res = await getTime()
+      setTime(res.cur_date)
+    }
 
-    _getTime();
-  }, []);
+    _getTime()
+  }, [])
 
   return authState === AuthState.SignedIn && user ? (
     <div className="App">
@@ -55,7 +56,7 @@ const App: React.FC = () => {
         <AmplifySignIn slot="sign-in" hideSignUp={true} />
       </AmplifyAuthenticator>
     </AmplifyAuthContainer>
-  );
-};
+  )
+}
 
-export default App;
+export default App
