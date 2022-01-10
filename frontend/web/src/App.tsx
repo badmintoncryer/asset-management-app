a4dda63343fe6455ef6f213f7f440baa7402ea47
kazuho cryershinozuka <cryershinozuka.kb@om.asahi-kasei.co.jp>
2022-01-10 18:01:29 +0900

    [added] AppBar

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,4 +1,14 @@
 import React, { useState, useEffect } from 'react'
+import {
+  // makeStyles,
+  createTheme,
+  ThemeProvider,
+  Theme,
+} from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { BrowserRouter } from 'react-router-dom'
+
+import AppBar from './components/AppBar'
 import logo from './logo.svg'
 import './App.css'
 
@@ -22,6 +32,23 @@ Amplify.configure({
 export const apiEndpoint =
   'https://r4rj7apo23.execute-api.ap-northeast-1.amazonaws.com' // Please change this value. (Don't include '/api')
 
+const theme: Theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+})
+
 const App: React.FC = () => {
   const [authState, setAuthState] = useState<AuthState>()
   const [user, setUser] = useState<object | undefined>()
@@ -44,12 +71,18 @@ const App: React.FC = () => {
   }, [])
 
   return authState === AuthState.SignedIn && user ? (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Update Time: {time}</p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <AppBar />
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Update Time: {time}</p>
+          </header>
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   ) : (
     <AmplifyAuthContainer>
       <AmplifyAuthenticator>
