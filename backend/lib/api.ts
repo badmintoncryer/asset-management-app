1b82a59dabc299c8c294107be342229e378d54a2
kazuho cryershinozuka <cryershinozuka.kb@om.asahi-kasei.co.jp>
2022-01-22 16:36:06 +0900

    [removed] WAF

--- backend/lib/api.ts
+++ backend/lib/api.ts
@@ -2,7 +2,7 @@ import * as cdk from "@aws-cdk/core";
 import * as cognito from "@aws-cdk/aws-cognito";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as lambdaNodejs from "@aws-cdk/aws-lambda-nodejs";
-import * as waf from "@aws-cdk/aws-wafv2";
+// import * as waf from "@aws-cdk/aws-wafv2";
 import * as agw from "@aws-cdk/aws-apigateway";
 
 interface APIStackProps extends cdk.StackProps {
@@ -31,58 +31,58 @@ export class APIStack extends cdk.Stack {
       "allowedIpAddressRanges"
     );
 
-    const wafIPSet = new waf.CfnIPSet(this, `IPSet`, {
-      name: "BackendWebAclIpSet",
-      ipAddressVersion: "IPV4",
-      scope: "REGIONAL",
-      addresses: ipRanges,
-    });
+    // const wafIPSet = new waf.CfnIPSet(this, `IPSet`, {
+    //   name: "BackendWebAclIpSet",
+    //   ipAddressVersion: "IPV4",
+    //   scope: "REGIONAL",
+    //   addresses: ipRanges,
+    // });
 
-    const apiWaf = new waf.CfnWebACL(this, "waf", {
-      defaultAction: { block: {} },
-      scope: "REGIONAL",
-      visibilityConfig: {
-        cloudWatchMetricsEnabled: true,
-        sampledRequestsEnabled: true,
-        metricName: "ApiGatewayWAF",
-      },
-      // https://docs.aws.amazon.com/ja_jp/waf/latest/developerguide/aws-managed-rule-groups-list.html
-      rules: [
-        // AWSManagedRulesCommonRuleSet
-        {
-          priority: 1,
-          overrideAction: { none: {} },
-          visibilityConfig: {
-            sampledRequestsEnabled: true,
-            cloudWatchMetricsEnabled: true,
-            metricName: "AWS-AWSManagedRulesCommonRuleSet",
-          },
-          name: "AWSManagedRulesCommonRuleSet",
-          statement: {
-            managedRuleGroupStatement: {
-              vendorName: "AWS",
-              name: "AWSManagedRulesCommonRuleSet",
-            },
-          },
-        },
-        // AWSManagedRulesKnownBadInputsRuleSet
-        {
-          priority: 2,
-          name: "BackendWebAclIpRuleSet",
-          action: { allow: {} },
-          visibilityConfig: {
-            sampledRequestsEnabled: true,
-            cloudWatchMetricsEnabled: true,
-            metricName: "BackendWebAclIpRuleSet",
-          },
-          statement: {
-            ipSetReferenceStatement: {
-              arn: wafIPSet.attrArn,
-            },
-          },
-        },
-      ],
-    });
+    // const apiWaf = new waf.CfnWebACL(this, "waf", {
+    //   defaultAction: { block: {} },
+    //   scope: "REGIONAL",
+    //   visibilityConfig: {
+    //     cloudWatchMetricsEnabled: true,
+    //     sampledRequestsEnabled: true,
+    //     metricName: "ApiGatewayWAF",
+    //   },
+    //   // https://docs.aws.amazon.com/ja_jp/waf/latest/developerguide/aws-managed-rule-groups-list.html
+    //   rules: [
+    //     // AWSManagedRulesCommonRuleSet
+    //     {
+    //       priority: 1,
+    //       overrideAction: { none: {} },
+    //       visibilityConfig: {
+    //         sampledRequestsEnabled: true,
+    //         cloudWatchMetricsEnabled: true,
+    //         metricName: "AWS-AWSManagedRulesCommonRuleSet",
+    //       },
+    //       name: "AWSManagedRulesCommonRuleSet",
+    //       statement: {
+    //         managedRuleGroupStatement: {
+    //           vendorName: "AWS",
+    //           name: "AWSManagedRulesCommonRuleSet",
+    //         },
+    //       },
+    //     },
+    //     // AWSManagedRulesKnownBadInputsRuleSet
+    //     {
+    //       priority: 2,
+    //       name: "BackendWebAclIpRuleSet",
+    //       action: { allow: {} },
+    //       visibilityConfig: {
+    //         sampledRequestsEnabled: true,
+    //         cloudWatchMetricsEnabled: true,
+    //         metricName: "BackendWebAclIpRuleSet",
+    //       },
+    //       statement: {
+    //         ipSetReferenceStatement: {
+    //           arn: wafIPSet.attrArn,
+    //         },
+    //       },
+    //     },
+    //   ],
+    // });
 
     // Definition of API Gateway
     const api = new agw.RestApi(this, "api", {
@@ -99,10 +99,10 @@ export class APIStack extends cdk.Stack {
     const region = cdk.Stack.of(this).region;
     const restApiId = api.restApiId;
     const stageName = api.deploymentStage.stageName;
-    new waf.CfnWebACLAssociation(this, "apply-waf-apigw", {
-      webAclArn: apiWaf.attrArn,
-      resourceArn: `arn:aws:apigateway:${region}::/restapis/${restApiId}/stages/${stageName}`,
-    });
+    // new waf.CfnWebACLAssociation(this, "apply-waf-apigw", {
+    //   webAclArn: apiWaf.attrArn,
+    //   resourceArn: `arn:aws:apigateway:${region}::/restapis/${restApiId}/stages/${stageName}`,
+    // });
 
     // GET: /time
     const userinfo = api.root.addResource("time");
